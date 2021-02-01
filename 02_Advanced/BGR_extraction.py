7daf9db8b160b0243923022b9710c9959bd85afd
Dan Sato <s19C1054BU@s.chibakoudai.jp>
2021-02-01 12:12:25 +0900

    add sample_codes

---
+++ 02_Advanced/BGR_extraction.py
@@ -0,0 +1,43 @@
+#!/usr/bin/enz python3
+#! -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+def green_extraction(img):
+    
+    BGR_min = np.array([0, 0, 0])
+    BGR_max = np.array([60, 255, 128])
+
+    img_mask = cv2.inRange(img, BGR_min, BGR_max)
+
+    green_img = cv2.bitwise_and(img, img, mask = img_mask)
+
+    return green_img
+
+def blue_extraction(img):
+    
+    BGR_min = np.array([140, 0, 0])
+    BGR_max = np.array([255, 255, 150])
+
+    img_mask = cv2.inRange(img, BGR_min, BGR_max)
+
+    blue_img = cv2.bitwise_and(img, img, mask = img_mask)
+
+    return blue_img
+
+def main():
+
+    img = cv2.imread("images/sea.jpg")
+
+    green = green_extraction(img)
+    blue = blue_extraction(img)
+
+    cv2.imshow("origin", img)
+    cv2.imshow("green", green)
+    cv2.imshow("blue", blue)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ 02_Advanced/HSV_extraction.py
@@ -0,0 +1,47 @@
+#!/usr/bin/enz python3
+#! -*- coding: utf-8 -*-
+
+import cv2
+import numpy as np
+
+def green_extraction(img, hsv_img):
+    
+    hsv_min = np.array([30, 64, 0])
+    hsv_max = np.array([90, 255, 255])
+
+    img_mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
+
+    green_img = cv2.bitwise_and(img, img, mask = img_mask)
+
+    return green_img
+
+def blue_extraction(img, hsv_img):
+    
+    hsv_min = np.array([90, 0, 0])
+    hsv_max = np.array([150, 255, 255])
+
+    img_mask = cv2.inRange(hsv_img, hsv_min, hsv_max)
+
+    blue_img = cv2.bitwise_and(img, img, mask = img_mask)
+
+    return blue_img
+
+def main():
+
+    img = cv2.imread("images/sea.jpg")
+
+    hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+    green = green_extraction(img, hsv_img)
+    blue = blue_extraction(img, hsv_img)
+
+    cv2.imshow("origin", img)
+    cv2.imshow("hsv", hsv_img)
+    cv2.imshow("green", green)
+    cv2.imshow("blue", blue)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
